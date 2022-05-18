fc47a3221e2bd34a0106a5aa21e5854c41baba0a
mokaim <mokaim@naver.com>
2022-05-18 10:19:30 +0900

    AuthFactory 클래스 추가

--- user/src/main/java/io/github/mokaim/user/service/auth/AuthFactory.java
+++ user/src/main/java/io/github/mokaim/user/service/auth/AuthFactory.java
@@ -1,16 +1,20 @@
 package io.github.mokaim.user.service.auth;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class AuthFactory {
+
 
   public static AuthService getInstance(String authType) {
     if ("PIN".equals(authType)) {
       return new PinAuthService();
     } else if ("PHONE".equals(authType)) {
-
+      return new PhoneAuthService();
     } else if ("OTP".equals(authType)) {
-
+      return new OtpAuthService();
     } else if ("NONE".equals(authType)) {
-
+      return new BasicAuthService();
     }
     return null;
   }
